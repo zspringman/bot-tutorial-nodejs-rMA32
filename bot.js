50d166224561ee932ee42d28545e2f1e5c317f24
zspringman <zack.springman@gmail.com>
2016-03-09 13:54:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,7 @@ function respond() {
       botRegxJon2 = /^\/fag/;
       botRegexBlurry = /^\/faggot/;
       botRegexNum2 = /^\/p#2/;
+      botRegexWest = /^\/west;
       
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -33,6 +34,11 @@ function respond() {
   else if(request.text && botRegexSalt.test(request.text)) {
     this.res.writeHead(200);
     postMessage("https://i.imgur.com/B5BSVqH.png");
+    this.res.end();
+  }
+  else if(request.text && botRegexWest.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://images.huffingtonpost.com/2015-02-09-oKANYEWESTfacebook.jpg");
     this.res.end();
   }
   else if(request.text && botRegexJon.test(request.text)) {

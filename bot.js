443d1c7a62b422b17860bcf5ca0df1e482ef9c62
zspringman <zack.springman@gmail.com>
2016-03-29 22:09:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ function respond() {
       botRegexWest = /^\/west/;
       botRegexTroll = /^\/troll/;
       botRegexStg = /^\/stg/;
-      botRegexInformation = /^\/inf/;
+      botRegexInformation = /^\/info/;
       
       
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
